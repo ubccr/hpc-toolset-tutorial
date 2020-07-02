8d9270536fa4d2b2f3df47b6e42996ad988be9bc
Trey Dockendorf <tdockendorf@osc.edu>
2020-07-02 11:33:27 -0400

    Add ldap docker service

---
+++ ldap/install.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+set -e
+
+trap 'ret=$?; test $ret -ne 0 && printf "failed\n\n" >&2; exit $ret' EXIT
+
+log_info() {
+  printf "\n\e[0;35m $1\e[0m\n\n"
+}
+
+source /build/base.config
+
+#------------------------
+# Bootstrap LDAP OUs
+#------------------------
+mkdir -p /container/service/slapd/assets/config/bootstrap/ldif/custom
+cat > /container/service/slapd/assets/config/bootstrap/ldif/custom/0-ous.ldif <<EOF
+dn: ou=People,dc=example,dc=org
+objectClass: organizationalUnit
+ou: People
+
+dn: ou=Groups,dc=example,dc=org
+objectClass: organizationalUnit
+ou: Groups
+EOF
+
+#------------------------
+# Setup user accounts
+#------------------------
+
+idnumber=1000
+for uid in hpcadmin $USERS
+do
+    log_info "Adding LDIF for $uid user account with uidnumber $idnumber.."
+    passvar="PASSWD_$uid"
+    passwd=${!passvar:-ilovelinux}
+    cat > /container/service/slapd/assets/config/bootstrap/ldif/custom/1-$uid.ldif <<EOF
+dn: cn=${uid},ou=People,dc=example,dc=org
+objectClass: person
+objectClass: posixAccount
+objectClass: inetOrgPerson
+gecos: ${uid}
+cn: ${uid}
+sn: ${uid}
+uid: ${uid}
+homeDirectory: /home/${uid}
+uidNumber: ${idnumber}
+gidNumber: ${idnumber}
+mail: ${uid}@example.com
+userpassword: ${passwd}
+
+dn: cn=${uid},ou=Groups,dc=example,dc=org
+objectClass: posixGroup
+objectClass: groupOfMembers
+cn: ${uid}
+gidNumber: ${idnumber}
+member: cn=${uid},ou=People,dc=example,dc=org
+EOF
+    idnumber=$((idnumber + 1))
+done
