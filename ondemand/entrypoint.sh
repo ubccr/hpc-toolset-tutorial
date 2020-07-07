2343fb4dad2ac934e1f878ba0f03c602e93ddbbf
Trey Dockendorf <tdockendorf@osc.edu>
2020-07-07 07:57:57 -0400

    Switch OnDemand to Dex for authentication
    Update to Ondemand 1.8 through 'latest' repo
    The port used by OnDemand must be the same inside and outside the container to keep configs simple.
    Ensure CA cert is trusted for OnDemand as Dex does not seem to work with self-signed certs

--- ondemand/entrypoint.sh
+++ ondemand/entrypoint.sh
@@ -3,6 +3,9 @@ set -e
 
 if [ "$1" = "serve" ]
 then
+    echo "---> Populating /etc/ssh/ssh_known_hosts from frontend for ondemand..."
+    /usr/bin/ssh-keyscan frontend >> /etc/ssh/ssh_known_hosts
+
     echo "---> Starting SSSD on ondemand ..."
     /sbin/sssd --logger=stderr -d 3 -i 2>&1 &
 
@@ -11,6 +14,9 @@ then
 
     echo "---> Starting sshd on ondemand..."
     /usr/sbin/sshd -e
+
+    echo "---> Starting ondemand-dex..."
+    gosu ondemand-dex /usr/sbin/ondemand-dex serve /etc/ood/dex/config.yaml &
 
     echo "---> Starting ondemand httpd24..."
     /opt/rh/httpd24/root/usr/sbin/httpd-scl-wrapper -DFOREGROUND
--- ondemand/install.sh
+++ ondemand/install.sh
@@ -10,50 +10,88 @@ log_info() {
 log_info "Installing required packages for Ondemand.."
 yum install -y \
     centos-release-scl \
-    https://yum.osc.edu/ondemand/1.7/ondemand-release-web-1.7-1.noarch.rpm
+    https://yum.osc.edu/ondemand/latest/ondemand-release-web-latest-1-6.noarch.rpm
 
 yum install -y \
     ondemand \
-    openssl \
-    mod_authnz_pam
+    ondemand-dex \
+    openssl
 
 log_info "Setting up Ondemand"
 mkdir -p /etc/ood/config/clusters.d
 mkdir -p /etc/ood/config/apps/shell
 echo "DEFAULT_SSHHOST=frontend" > /etc/ood/config/apps/shell/env
 
-
-log_info "Configuring Ondemand for PAM based logins.."
-
-# This is to allow ondemand to use pam for local logins in docker:
-chmod 640 /etc/shadow
-chgrp apache /etc/shadow
-
-echo "LoadModule authnz_pam_module modules/mod_authnz_pam.so" > /opt/rh/httpd24/root/etc/httpd/conf.modules.d/55-authnz_pam.conf
-cp /usr/lib64/httpd/modules/mod_authnz_pam.so /opt/rh/httpd24/root/usr/lib64/httpd/modules/
-cp /etc/pam.d/sshd /etc/pam.d/ood-webapp
+log_info "Configuring Ondemand ood_portal.yml .."
 
 tee /etc/ood/config/ood_portal.yml <<EOF
 ---
 #
 # Portal configuration
 #
+listen_addr_port:
+  - '3443'
+servername: localhost
+port: 3443
 ssl:
   - 'SSLCertificateFile "/etc/pki/tls/certs/ood.crt"'
   - 'SSLCertificateKeyFile "/etc/pki/tls/private/ood.key"'
-auth:
-  - 'AuthType Basic'
-  - 'AuthName "HPC Tutorial OnDemand"'
-  - 'AuthBasicProvider PAM'
-  - 'AuthPAMService ood-webapp'
-  - 'Require valid-user'
+oidc_scope: "openid profile email groups"
+dex:
+  connectors:
+    - type: ldap
+      id: ldap
+      name: LDAP
+      config:
+        host: ldap:636
+        insecureSkipVerify: true
+        bindDN: cn=admin,dc=example,dc=org
+        bindPW: admin
+        userSearch:
+          baseDN: ou=People,dc=example,dc=org
+          filter: "(objectClass=posixAccount)"
+          username: uid
+          idAttr: uid
+          emailAttr: mail
+          nameAttr: gecos
+          preferredUsernameAttr: uid
+        groupSearch:
+          baseDN: ou=Groups,dc=example,dc=org
+          filter: "(objectClass=posixGroup)"
+          userMatchers:
+            - userAttr: DN
+              groupAttr: member
+          nameAttr: cn
+  # This is the default, but illustrating how to change
+  frontend:
+    theme: ondemand
 EOF
 
-# Create self-signed ssl cert
 log_info "Creating self-signed ssl cert for ondemand.."
-openssl req -x509 -nodes -days 365 -subj "/C=US/ST=NY/O=HPC Tutorial/CN=ondemand" -newkey rsa:2048 -keyout /etc/pki/tls/private/ood.key -out /etc/pki/tls/certs/ood.crt
+# Dex expects a trusted CA cert is used
+# Generate CA
+openssl genrsa -out /etc/pki/tls/ca.key 4096
+openssl req -new -x509 -days 3650 -sha256 -key /etc/pki/tls/ca.key -extensions v3_ca -out /etc/pki/tls/ca.crt -subj "/CN=fake-ca"
+# Generate certificate request
+openssl genrsa -out /etc/pki/tls/private/ood.key 2048
+openssl req -new -sha256 -key /etc/pki/tls/private/ood.key -out /etc/pki/tls/certs/ood.csr -subj "/C=US/ST=NY/O=HPC Tutorial/CN=ondemand"
+# Config for signing cert
+cat > /etc/pki/tls/ood.ext << EOF
+authorityKeyIdentifier=keyid,issuer
+basicConstraints=CA:FALSE
+keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
+subjectAltName = @alt_names
+[alt_names]
+DNS.1 = ondemand
+DNS.2 = localhost
+EOF
+# Sign cert request and generate cert
+openssl x509 -req -in /etc/pki/tls/certs/ood.csr -CA /etc/pki/tls/ca.crt -CAkey /etc/pki/tls/ca.key -CAcreateserial -out /etc/pki/tls/certs/ood.crt -days 365 -sha256 -extfile /etc/pki/tls/ood.ext
+# Add CA to trust store
+cp /etc/pki/tls/ca.crt /etc/pki/ca-trust/source/anchors/
+update-ca-trust extract
 
-log_info "Generating new httpd24 configs.."
+log_info "Generating new httpd24 and dex configs.."
 /opt/ood/ood-portal-generator/sbin/update_ood_portal
 
 yum clean all
